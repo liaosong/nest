8a168c012f478ae6bc5b530da7abe1684863f5a2
liaosong <liaosong1015@gmail.com>
2017-08-31 14:10:19 +0800

    refactor: remove unuse code

---
+++ src/apps/web/main.browser.ts
@@ -0,0 +1,40 @@
+/**
+ * Angular bootstrapping
+ */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { decorateModuleRef } from './app/environment';
+
+/**
+ * App Module
+ * our top level module that holds all of our components
+ */
+import { AdminModule } from './app/admin.module';
+
+/**
+ * Bootstrap our Angular app with a top level NgModule
+ */
+export function main(): Promise<any> {
+  return platformBrowserDynamic()
+    .bootstrapModule(AdminModule)
+    .then(decorateModuleRef)
+    .catch((err) => console.error(err));
+}
+
+/**
+ * Needed for hmr
+ * in prod this is replace for document ready
+ */
+switch (document.readyState) {
+  case 'loading':
+    document.addEventListener('DOMContentLoaded', _domReadyHandler, false);
+    break;
+  case 'interactive':
+  case 'complete':
+  default:
+    main();
+}
+
+function _domReadyHandler() {
+ document.removeEventListener('DOMContentLoaded', _domReadyHandler, false);
+ main();
+}
